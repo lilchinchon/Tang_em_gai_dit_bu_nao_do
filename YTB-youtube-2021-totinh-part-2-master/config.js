365560965dadfbfbd3d41873d530da9ec8f1e332
lilchinchon <110262988+lilchinchon@users.noreply.github.com>
2022-07-29 16:38:31 -0500

    Add files via upload

---
+++ YTB-youtube-2021-totinh-part-2-master/config.js
@@ -0,0 +1,19 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const
+CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Tao có điều này muốn hỏi mày, Mày nhớ phải trả lời thật lòng nha con đỹ`,
+    btnIntro: 'OK',
+    title: 'Tao biết M thích T phải không phải không ? 😙',
+    desc: 'M Đéo trả lời hoặc thoát ra, tức là M thích T đó nha :> ',
+    btnYes: 'Có, M thật sự thích T 💖💖💖',
+    btnNo: '💔 Có Cái Con Cac ! 💔 ',
+    question:'Nói cho Tao biết đi vì sao m thích t 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Vì T thik mấy thag thiểu năng dễ bị lừa 😊😊',
+    mess: 'Tao biết mà, Tao cũng thích Mày vailonnnn 🥰. ',
+    messDesc: '💬💬 Nhắn tin cho Tao nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/lil.ching.chong'
+}
